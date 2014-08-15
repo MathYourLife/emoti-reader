8b74fa60467888593c2ba765e530f06eb1560083
dcouture <dcouture@dyn.com>
2014-08-15 00:05:05 -0400

    adding zookeeper and kafka base installs

--- chef/emoti_reader/recipes/default.rb
+++ chef/emoti_reader/recipes/default.rb
@@ -11,5 +11,21 @@ include_recipe 'heka'
 
 template '/etc/heka/readin.toml' do
   source 'heka/readin.toml.erb'
+  variables(
+    :readin_port => node["emoti_reader"]["readin"]["port"]
+  )
   notifies :restart, 'service[heka]'
+end
+
+include_recipe 'runit'
+include_recipe 'zookeeper'
+include_recipe 'zookeeper::service'
+include_recipe 'kafka'
+
+template '/opt/kafka/config/server.properties' do
+  source 'kafka/server.properties.erb'
+  variables(
+    :kafka_port => node["emoti_reader"]["kafka"]["port"]
+  )
+  notifies :restart, 'service[kafka]'
 end
